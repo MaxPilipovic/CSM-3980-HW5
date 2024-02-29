7b0bd100c78f8a35637211a7fbadd8f7b787df13
MaxPilipovic <maxpkusa@gmail.com>
2024-02-28 17:01:10 -0800

    - Finished ready for testing (25 - 125)

--- Main2.java
+++ Main2.java
@@ -12,11 +12,11 @@ public class Main2 {
     private static final Random random = new Random();
     private static final int time = 100;
     private static final AtomicInteger threads = new AtomicInteger();
-    private static final int totalThreads = 625;
+    private static final int totalThreads = 125;
     //private static final Semaphore cleaners = new Semaphore();
     //private static final Semaphore polishers = new Semaphore();
     //private static final ReentrantLock lock = new ReentrantLock(true);
-    private static final Semaphore maxWorkers = new Semaphore(125, true);
+    private static final Semaphore maxWorkers = new Semaphore(25, true);
     private static final Lightswitch room = new Lightswitch();
 
 
